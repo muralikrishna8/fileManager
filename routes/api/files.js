7636e5a4f2ffadaff16556deed63d6826835a808
Murali Krishna <muralikv@thoughtworks.com>
2015-11-08 00:32:39 +0530

    API - changed path for supporing cross-platform

--- routes/api/files.js
+++ routes/api/files.js
@@ -1,8 +1,10 @@
 var router = require("express").Router();
+var path = require("path");
 var fs = require("fs");
 router.get('/', function(req, res, next) {
-    var path = req.query.path || '';
-    var readFiles = fs.readdir(process.env.HOME + "/" + path, function(err, files) {
+    var homedir = (process.platform === 'win32') ? process.env.HOMEPATH : process.env.HOME;
+    var filePath = path.join(homedir, req.query.path || "");
+    var readFiles = fs.readdir(filePath, function(err, files) {
         if (err) {
             console.log(err);
             res.sendStatus(403);
