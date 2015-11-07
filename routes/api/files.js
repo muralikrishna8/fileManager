b3c77ff046848100b3be5bf948a93c4032313947
Murali Krishna <muralikv@thoughtworks.com>
2015-11-07 12:10:44 +0530

    API - added query path for list files - can give specific path to get the files

--- routes/api/files.js
+++ routes/api/files.js
@@ -1,7 +1,8 @@
 var router = require("express").Router();
 var fs = require("fs");
 router.get('/', function(req, res, next) {
-    var readFiles = fs.readdir(process.env.HOME, function(err, files) {
+    var path = req.query.path || '';
+    var readFiles = fs.readdir(process.env.HOME + "/" + path, function(err, files) {
         if (err) {
             console.log(err);
             res.sendStatus(403);
