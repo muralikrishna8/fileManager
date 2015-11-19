5447674ea22dd1e15b02d5f7c39cb3af9c191308
Murali Krishna <muralikv@thoughtworks.com>
2015-11-19 17:37:45 +0530

    API | added validation for file not found

--- routes/api/files.js
+++ routes/api/files.js
@@ -1,20 +1,25 @@
 var router = require("express").Router();
 var path = require("path");
 var fs = require("fs");
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
     var homedir = (process.platform === 'win32') ? process.env.HOMEPATH : process.env.HOME;
     var filePath = path.join(homedir, req.query.path || "");
-    var readFiles = fs.readdir(filePath, function(err, files) {
-        if (err) {
+    var readFiles = fs.readdir(filePath, function (err, files) {
+        if (err && err.code === 'ENOENT') {
+            res.status(404);
+            res.json({
+                "message": "File not exist"
+            });
             console.log(err);
-            res.sendStatus(403);
         }
-        files = files.map(function(file){
-            var stats =  fs.statSync(path.join(filePath, file));
-            return {"name" : file, "isDir" : stats.isDirectory()};
-        });
-
-        res.json(files);
+        else {
+            files = files.map(function (file) {
+                var stats = fs.statSync(path.join(filePath, file));
+                return {"name": file, "isDir": stats.isDirectory()};
+            });
+            res.status(200);
+            res.json(files);
+        }
     })
 });
 
